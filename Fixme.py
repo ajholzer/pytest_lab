50e51e43a0e0e8f6d8406e7560b05a8442b605ee
Maxine Tamas <mtamas25@cmc.edu>
2023-02-04 15:26:47 -0800

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   Fixme.py

--- Fixme.py
+++ Fixme.py
@@ -5,7 +5,7 @@ def evens(n):
     Returns a list of even numbers from 0 to n inclusive.
     '''
     arr = []
-    for i in range(0,n):
+    for i in range(0,n+1):
         if i%2 == 0:
             arr.append[i]
     return arr
